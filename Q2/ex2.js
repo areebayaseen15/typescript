93becbb96497f76d4e774467b9cd052234adab67
areebayaseen15 <147875287+areebayaseen15@users.noreply.github.com>
2024-03-05 00:05:58 +0500

    Add files via upload
    solve qno2. 
    #governor it course #typescript #45assignment questions #solve qno2.

---
+++ Q2/ex2.js
@@ -0,0 +1,2 @@
+var FirstName = 'Areeba Yaseen';
+console.log("Assalam o alaikum ".concat(FirstName, ", would you like to learn some Python today?"));
---
+++ Q2/exc2.ts
@@ -0,0 +1,2 @@
+let FirstName: string = 'Areeba Yaseen';
+console.log(`Assalam o alaikum ${FirstName}, would you like to learn some Python today?`)
