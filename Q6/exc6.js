7a5e2776141e5bb3104deda60f4d5ddb46286b28
areebayaseen15 <147875287+areebayaseen15@users.noreply.github.com>
2024-03-08 23:18:45 +0500

    Add files via upload

---
+++ Q6/exc6.js
@@ -0,0 +1,8 @@
+var whiteSpace = 'Areeba\tYaseen';
+console.log(whiteSpace);
+var whiteSpace2 = 'Areeba\nYaseen';
+console.log(whiteSpace2);
+var personName = "\n\t Areeba Yaseen\t\n";
+console.log(personName);
+var stripped = personName.trim();
+console.log(stripped);
---
+++ Q6/exc6.ts
@@ -0,0 +1,16 @@
+//example2
+
+let whiteSpace:string='Areeba\tYaseen';
+console.log(whiteSpace);
+
+let whiteSpace2:string='Areeba\nYaseen'
+console.log(whiteSpace2);
+
+
+
+//Example2
+
+let personName:string=`\n\t Areeba Yaseen\t\n`
+console.log(personName);
+let stripped=personName.trim()
+console.log(stripped);
